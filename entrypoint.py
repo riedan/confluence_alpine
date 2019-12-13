629f435ec610a7e9259f3bf9058b7e3d498364f4
x171860 Rony JEAN SIMON <rony.jean-simon-ext@socgen.com>
2019-12-13 17:14:00 +0100

    xml edit

--- entrypoint.py
+++ entrypoint.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import shutil
+import xml.dom.minidom
 from entrypoint_helpers import env, gen_cfg, str2bool, start_app, set_perms, set_ownership
 
 
@@ -7,6 +8,8 @@ RUN_USER = env['run_user']
 RUN_GROUP = env['run_group']
 CONFLUENCE_INSTALL_DIR = env['confluence_install_dir']
 CONFLUENCE_HOME = env['confluence_home']
+SSL_ENABLED = env['atl_sslenabled']
+
 
 gen_cfg('server.xml.j2', f'{CONFLUENCE_INSTALL_DIR}/conf/server.xml')
 gen_cfg('seraph-config.xml.j2',
@@ -16,7 +19,12 @@ gen_cfg('confluence-init.properties.j2',
 gen_cfg('confluence.cfg.xml.j2', f'{CONFLUENCE_HOME}/confluence.cfg.xml',
         user=RUN_USER, group=RUN_GROUP, overwrite=False)
 
-gen_cfg('web.xml.j2', f'{CONFLUENCE_INSTALL_DIR}/confluence/WEB-INF/web.xml')
+# parse an xml file by name
+mydoc = minidom.parse(f'{CONFLUENCE_INSTALL_DIR}/confluence/WEB-INF/web.xml')
+
+new_security_constraint = dom.createElement('security-constraint')
+new_security_constraint.setAttribute('Include', 'newpath')
+
 
 
 set_ownership(f'{CONFLUENCE_INSTALL_DIR}/logs',  user=RUN_USER, group=RUN_GROUP)
